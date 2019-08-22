f0b862eefd9c0268c34405bd1e79ed9b2eac50de
Aluno <aluno@naverecife.local>
2019-08-22 12:23:11 -0300

    primeiro commit

---
+++ Assets/BackGorund.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGorund : MonoBehaviour
+{
+    [SerializeField] GameObject cenario;
+    [SerializeField] Vector2 posiSpawn;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        MoverCenario();
+        if(transform.position.x < -18.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void MoverCenario()
+    {
+        transform.Translate(Vector2.left * 4 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Instantiate(cenario, posiSpawn, Quaternion.identity);
+        }
+    }
+}
