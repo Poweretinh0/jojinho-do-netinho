971038876d59e851996f02516f59d765e26342c5
Roberto <robertoescola3@gmail.com>
2019-08-26 16:54:57 -0300

    looping

---
+++ Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject verificaplataforma;
+    [SerializeField]
+    GameObject plataforma;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(player.transform.position.x > verificaplataforma.transform.position.x)
+        {
+
+            verificaplataforma.transform.position = new Vector2(player.transform.position.x + 6.7f, 0);
+
+            Instantiate(plataforma, verificaplataforma.transform.position, Quaternion.identity);
+
+            
+        }
+    }
+}
